05d33f7c794153d536fac89ed95b4c744a8d5fd1
Youtube <Youtube>
2025-01-16 15:43:53 +0000

    Ver.0.1

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-gh dghdgdghdgh
+def compute_gcd(x, y):
+	gcd = False
+	if x and y != 0:
+		if x > y:                                   # compare who large
+			small = y
+		else:
+			small = x
+		for i in range(1, small + 1):               # forloop
+			if (x % i == 0) and (y % i == 0):
+				gcd = i
+	else:
+		print("Input can't be ZERO!!!")
+	return gcd
