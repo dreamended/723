2c4270a7a913ca8ed8d34a419cf874f06735d55d
Youtube <Youtube>
2025-01-26 20:18:49 +0000

    Ver.0.3

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-x = int(input("enter the first number"))                #enter number
-y = int(input("enter the second number"))
+def caesar_encrypt(number, shift):          #use caesar cipher
+	return (number + shift) % 256               #encryption
+def caesar_decrypt(number, shift):          #decryption
+	return (number - shift) % 256
+
+x = int(input("enter the first number: "))                #enter number
+y = int(input("enter the second number: "))
+shift = 6           #the amount that will be displaced
+
 def compute_gcd(x, y):
 	gcd = False
 	if x and y != 0:                    #neither number can be zero
@@ -14,4 +21,13 @@ def compute_gcd(x, y):
 		print("Input can't be ZERO!!!")
 	return gcd
 result = compute_gcd(x, y)
-print(result)
+
+x_encrypted = caesar_encrypt(x, shift)          #encrypt the input numbers
+y_encrypted = caesar_encrypt(y, shift)
+
+gcd_encrypted = compute_gcd(x_encrypted, y_encrypted)           #calculate the greatest common divisor after encryption
+
+gcd_decrypted = caesar_decrypt(gcd_encrypted, shift)            #decryption the result
+
+print(f"encrypt : {gcd_encrypted}")
+print(f"decryption : {gcd_decrypted}")
